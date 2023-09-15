dde44757f24d965d7c338647110c7a7889a45b45
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-09-14 21:52:25 -0400

    add two numbers

--- leetcode/addtwonumbers.go
+++ leetcode/addtwonumbers.go
@@ -1,21 +1,29 @@
 package leetcode
 
-import "fmt"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	current := l1
-	first := []int{}
-	for current != nil {
-		fmt.Println(current.Val)
-		first = append(first, current.Val)
-		current = current.Next
+	result := &ListNode{}
+	tmp := result
+	for l1 != nil || l2 != nil {
+		if l1 != nil {
+			tmp.Val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			tmp.Val += l2.Val
+			l2 = l2.Next
+		}
+		if tmp.Val > 9 {
+			tmp.Val -= 10
+			tmp.Next = &ListNode{Val: 1}
+		} else if l1 != nil || l2 != nil {
+			tmp.Next = &ListNode{}
+		}
+		tmp = tmp.Next
 	}
-	reverseSlice(first)
-	fmt.Println("first ", first)
-	return &ListNode{}
+	return result
 }
