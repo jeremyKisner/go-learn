5ac758a483d06a43db19f7fb92068f02878596ac
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-02 21:36:06 -0400

    setting up for challenge

---
+++ leetcode/mergesortedarray_test.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMerge(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums1    []int
+		m        int
+		nums2    []int
+		n        int
+		expected []int
+	}{
+		{
+			name:     "test",
+			nums1:    []int{1, 2, 3, 0, 0, 0},
+			m:        3,
+			nums2:    []int{2, 5, 6},
+			n:        3,
+			expected: []int{1, 2, 2, 3, 5, 6},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := Merge(tt.nums1, tt.m, tt.nums2, tt.n)
+			assert.Equal(t, tt.expected, res)
+		})
+	}
+}
