b3133ba88c1f8d361f88cd8b3921a1ac51a69d1d
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-04 22:12:14 -0400

    finish first pass

--- hackerrank/breakingrecords.go
+++ hackerrank/breakingrecords.go
@@ -2,6 +2,28 @@ package hackerrank
 
 func BreakingRecords(scores []int32) []int32 {
 	var res []int32
+	var numOfBrokenRecords int32
+	var numOfFailedAttempts int32
+	var minimum int32
+	var maximum int32
 
+	for i := 0; i < len(scores); i++ {
+		newScore := scores[i]
+		if minimum == 0 && maximum == 0 {
+			minimum = newScore
+			maximum = newScore
+			continue
+		}
+
+		if newScore > maximum {
+			maximum = newScore
+			numOfBrokenRecords++
+		} else if newScore < minimum {
+			minimum = newScore
+			numOfFailedAttempts++
+		}
+	}
+
+	res = append(res, numOfBrokenRecords, numOfFailedAttempts)
 	return res
 }
