a7ff0b43a129ad3de12a0af3624dea89f3ad2abb
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-04 22:15:11 -0400

    added new unit test

--- hackerrank/breakingrecords_test.go
+++ hackerrank/breakingrecords_test.go
@@ -16,6 +16,11 @@ func TestBreakingRecords(t *testing.T) {
 			scores: []int32{10, 5, 20, 20, 4, 5, 2, 25, 1},
 			want:   []int32{2, 4},
 		},
+		{
+			name:   "test 2",
+			scores: []int32{3, 4, 21, 36, 10, 28, 35, 5, 24, 42},
+			want:   []int32{4, 0},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
