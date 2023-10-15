0c41be2e7102e2abf62dcf1c2a3e447233bc0d1b
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-14 22:51:25 -0400

    removed unneed guard clause after fixing bug

--- hackerrank/easy/subarraydivision.go
+++ hackerrank/easy/subarraydivision.go
@@ -1,40 +1,11 @@
 package hackerrank
 
-/*
- * Complete the 'birthday' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY s
- *  2. INTEGER d
- *  3. INTEGER m
- */
-
 // s = number of chocolates
 // d = birthday
 // m = birth month
 
-/*
- iterate the number of chocolates
- add up the current number
- if sum of current numbers equal d
- then count all the summed numbers equal m
-
- if sum of numbers is too low continue
-
- if sum is too much reset
-*/
-
 func Birthday(s []int32, d int32, m int32) int32 {
 	var res int32
-
-	// guard clause for a single item
-	if len(s) == 1 {
-		if s[0] == d && m == 1 {
-			res++
-			return res
-		}
-	}
 
 	for i := 0; i < len(s); i++ {
 		var sum int32
