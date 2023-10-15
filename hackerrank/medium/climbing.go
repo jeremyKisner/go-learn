950ce5a31b53824afee90685d5a155b3ddb37883
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-14 23:13:23 -0400

    setting up for new challenge

---
+++ hackerrank/medium/climbing.go
@@ -0,0 +1,19 @@
+package medium
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+
+
+
+ */
+
+func ClimbingLeaderboard(ranked []int32, player []int32) []int32 {
+	var res []int32
+
+	return res
+}
---
+++ hackerrank/medium/climbing_test.go
@@ -0,0 +1,29 @@
+package medium
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClimbingLeaderboard(t *testing.T) {
+	tests := []struct {
+		name     string
+		ranked   []int32
+		player   []int32
+		expected []int32
+	}{
+		{
+			name:     "test 1",
+			ranked:   []int32{100, 90, 90, 80},
+			player:   []int32{70, 80, 105},
+			expected: []int32{4, 3, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := ClimbingLeaderboard(tt.ranked, tt.player)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
