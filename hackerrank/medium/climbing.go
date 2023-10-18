3ac61f554dab35608691026f71024289775540b5
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-17 22:28:50 -0400

    finished first test case

--- hackerrank/medium/climbing.go
+++ hackerrank/medium/climbing.go
@@ -19,84 +19,38 @@ import "fmt"
 
 func ClimbingLeaderboard(ranked []int32, player []int32) []int32 {
 	var res []int32
-	ranked = BuildRanks(player, ranked)
-	fmt.Println(res)
-	leaderBoard := BuildLeaderBoard(ranked)
-	fmt.Println(leaderBoard)
-	res = ComparePlayerRanks(player, leaderBoard)
-	return res
-}
 
-func BuildRanks(player []int32, ranked []int32) []int32 {
-	for i := len(player) - 1; i >= 0; i-- {
-		start := 0
-		end := len(ranked) - 1
-		firstPlace := ranked[start]
-		lastPlace := ranked[end]
-		playerScore := player[i]
-		if playerScore >= firstPlace {
-			fmt.Println("new high score!")
-			ranked = append([]int32{playerScore}, ranked...)
-		} else if playerScore <= lastPlace {
-			fmt.Println("tough luck, do better next time!")
-			ranked = append(ranked, playerScore)
-		} else {
-			mid := end / 2
-			midPlace := ranked[mid]
-			if playerScore == midPlace {
-				fmt.Println("playerscore equals midplace")
-				ranked = append(ranked[:mid], append([]int32{(playerScore)}, ranked[mid:]...)...)
-			} else if playerScore > midPlace {
-				fmt.Println("playerscore >= than midplace")
-				for j := mid; j >= 0; j-- {
-					temp := ranked[j]
-					if playerScore > temp {
-						ranked = append(ranked[:j], append([]int32{playerScore}, ranked[j:]...)...)
-						break
-					}
-				}
-			} else {
-				fmt.Println("playerscore less than midplace")
-				for j := mid; j < len(ranked); j++ {
-					temp := ranked[j]
-					if playerScore > temp {
-						ranked = append(ranked[:j], append([]int32{playerScore}, ranked[j:]...)...)
-						break
-					}
-				}
-			}
-		}
-	}
-	fmt.Println(ranked)
-	return ranked
-}
-
-func BuildLeaderBoard(leaderboard []int32) map[int32]int32 {
-	var lbScr int32
-	rankings := make(map[int32]int32)
-	rank := 0
-	if len(leaderboard) > 0 {
-		lbScr = leaderboard[0]
-		rank++
-		rankings[lbScr] = int32(rank)
-	}
-	for i := 0; i < len(leaderboard); i++ {
-		lbScr = leaderboard[i]
-		if _, exists := rankings[lbScr]; !exists {
-			rank++
-			rankings[lbScr] = int32(rank)
-		}
-	}
-	return rankings
-}
-
-func ComparePlayerRanks(player []int32, leaderboard map[int32]int32) []int32 {
-	var playerRanks []int32
 	for i := 0; i < len(player); i++ {
 		playerScore := player[i]
-		rank := leaderboard[playerScore]
-		playerRanks = append(playerRanks, rank)
+		scoreFound := false
+		prevScore := int32(0)
+		rank := int32(0)
+		for j := 0; j < len(ranked); j++ {
+			r := ranked[j]
+			if prevScore == 0 {
+				prevScore = r
+				rank += 1
+			} else if r != prevScore {
+				rank += 1
+				prevScore = r
+			}
+			if playerScore >= r {
+				fmt.Println("index ", j, ":", playerScore, " greater or equal ", r)
+				res = append(res, rank)
+				ranked = append(ranked[:j], append([]int32{(playerScore)}, ranked[j:]...)...)
+				scoreFound = true
+				break
+			} else {
+				fmt.Println("index ", j, ":", playerScore, " less than ", r)
+			}
 
+		}
+
+		if !scoreFound {
+			rank++
+			ranked = append(ranked, rank)
+			res = append(res, rank)
+		}
 	}
-	return playerRanks
+	return res
 }
