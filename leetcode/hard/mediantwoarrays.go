8ab3a7c9ed2d1cc0ee2a533835fb9c0346139a2c
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-11-01 22:15:39 -0400

    setting up for challenge

---
+++ leetcode/hard/mediantwoarrays.go
@@ -0,0 +1,6 @@
+package hard
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var res float64
+	return res
+}
---
+++ leetcode/hard/mediantwoarrays_test.go
@@ -0,0 +1,28 @@
+package hard
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_findMedianSortedArrays(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums1    []int
+		nums2    []int
+		expected float64
+	}{
+		{
+			name:     "test 1",
+			nums1:    []int{1, 3},
+			nums2:    []int{2},
+			expected: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, findMedianSortedArrays(tt.nums1, tt.nums2))
+		})
+	}
+}
