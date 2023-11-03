bbcef9e7cb0a0de2a65f938e55bf91e06276022a
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-11-02 22:16:20 -0400

    pass first test

--- leetcode/hard/mediantwoarrays.go
+++ leetcode/hard/mediantwoarrays.go
@@ -1,6 +1,27 @@
-package hard
+package leetcode
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	var res float64
-	return res
+	var sorted []int
+
+	sorted = append(sorted, nums1...)
+	sorted = append(sorted, nums2...)
+
+	for i := len(sorted) - 1; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			first := sorted[j]
+			second := sorted[j+1]
+			if first > second {
+				sorted[j] = second
+				sorted[j+1] = first
+			}
+		}
+
+	}
+
+	mid := len(sorted) / 2
+	if len(sorted)%2 != 0 {
+		return float64(sorted[mid])
+	} else {
+		return float64(sorted[mid]) + float64(.5)
+	}
 }
