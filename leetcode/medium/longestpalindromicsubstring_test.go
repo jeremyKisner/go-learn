23d84dbc6bf02e86c1164bac815def5095c75e9b
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-11-15 22:29:24 -0500

    finish algorithm

--- leetcode/medium/longestpalindromicsubstring_test.go
+++ leetcode/medium/longestpalindromicsubstring_test.go
@@ -18,6 +18,21 @@ func TestLongestPalindrome(t *testing.T) {
 			s:    "babad",
 			want: "bab",
 		},
+		{
+			name: "test 2",
+			s:    "cbbd",
+			want: "bb",
+		},
+		{
+			name: "test 3",
+			s:    "a",
+			want: "a",
+		},
+		{
+			name: "test 4",
+			s:    "ac",
+			want: "a",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
