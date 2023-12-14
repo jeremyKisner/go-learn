fc04a2da73e4988fb3bf8bbec9a3e314eb05f480
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-12-13 21:34:58 -0500

    finish algorithm

--- leetcode/medium/distributecandy.go
+++ leetcode/medium/distributecandy.go
@@ -1,7 +1,14 @@
 package leetcode
 
 func DistributeCandies(n int, limit int) int64 {
-	var res int64
-
-	return res
+	var ans int64
+	for i := 0; i <= min(limit, n); i++ {
+		if float64(n-i)/2. > float64(limit) {
+			continue
+		}
+		e := min(limit, n-i)
+		s := n - i - e
+		ans += int64(e - s + 1)
+	}
+	return ans
 }
--- leetcode/medium/distributecandy_test.go
+++ leetcode/medium/distributecandy_test.go
@@ -1,26 +1,28 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestDistributeCandies(t *testing.T) {
 	tests := []struct {
-		name  string
-		n     int
-		limit int
-		want  int64
+		name     string
+		n        int
+		limit    int
+		expected int64
 	}{
 		{
-			name:  "test 1",
-			n:     5,
-			limit: 2,
-			want:  3,
+			name:     "test 1",
+			n:        5,
+			limit:    2,
+			expected: 3,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := DistributeCandies(tt.n, tt.limit); got != tt.want {
-				t.Errorf("DistributeCandies() = %v, want %v", got, tt.want)
-			}
+			assert.Equal(t, tt.expected, DistributeCandies(tt.n, tt.limit))
 		})
 	}
 }
