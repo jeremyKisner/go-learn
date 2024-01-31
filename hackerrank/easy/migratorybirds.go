e0a6b74d5e5d7ef343625d372e0a9fd944fe99b6
Jeremy Kisner <kisner.jeremy@protonmail.com>
2024-01-30 22:38:30 -0500

    get first test case working

--- hackerrank/easy/migratorybirds.go
+++ hackerrank/easy/migratorybirds.go
@@ -1,7 +1,30 @@
 package hackerrank
 
-func MigratoryBirds(arr []int32) int32 {
-	var t int32
+func findMax(input map[int32]int32) int32 {
+	var key, max int32
+	for k, v := range input {
+		if key == 0 && max == 0 {
+			key = k
+			max = v
+			continue
+		}
+		if v > max && k < key {
+			key = k
+			max = v
+		}
+	}
+	return key
+}
 
-	return t
+func MigratoryBirds(arr []int32) int32 {
+	bt := make(map[int32]int32)
+	for _, v := range arr {
+		value, ok := bt[v]
+		if !ok {
+			bt[v] = 1
+		} else {
+			bt[v] = value + 1
+		}
+	}
+	return findMax(bt)
 }
