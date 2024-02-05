8ddd1ba63663e38f6e21cc301b5257a3059c26e0
Jeremy Kisner <kisner.jeremy@protonmail.com>
2024-02-04 22:05:47 -0500

    finish alrgorithm

--- leetcode/hard/mergesortedlists.go
+++ leetcode/hard/mergesortedlists.go
@@ -6,6 +6,38 @@ type ListNode struct {
 }
 
 func MergeKLists(lists []*ListNode) *ListNode {
-	var l *ListNode
-	return l
+	numOfLists := len(lists)
+	if numOfLists == 0 {
+		return nil
+	}
+	curList := lists[0]
+	if numOfLists == 1 {
+		return curList
+	}
+	for i := 1; i < numOfLists; i++ {
+		curList = mergeList(curList, lists[i])
+	}
+	return curList
+}
+
+func mergeList(l1, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	current := head
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+			current = current.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+			current = current.Next
+		}
+	}
+	if l1 != nil {
+		current.Next = l1
+	} else if l2 != nil {
+		current.Next = l2
+	}
+	return head.Next
 }
--- leetcode/hard/mergesortedlists_test.go
+++ leetcode/hard/mergesortedlists_test.go
@@ -11,7 +11,65 @@ func TestMergeKLists(t *testing.T) {
 		name     string
 		lists    []*ListNode
 		expected *ListNode
-	}{}
+	}{
+		{
+			name: "test 1",
+			lists: []*ListNode{
+				{
+					Val: 1,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val:  5,
+							Next: nil,
+						},
+					},
+				},
+				{
+					Val: 1,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+				{
+					Val: 2,
+					Next: &ListNode{
+						Val:  6,
+						Next: nil,
+					},
+				},
+			},
+			expected: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val: 4,
+									Next: &ListNode{
+										Val: 5,
+										Next: &ListNode{
+											Val:  6,
+											Next: nil,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			require.Equal(t, tt.expected, MergeKLists(tt.lists))
