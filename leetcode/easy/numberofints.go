702063f4301f5f3d822cf47bee07b35e863ce100
Jeremy Kisner <kisner.jeremy@protonmail.com>
2024-04-10 22:32:50 -0400

    solution

--- leetcode/easy/numberofints.go
+++ leetcode/easy/numberofints.go
@@ -1,30 +1,46 @@
 package leetcode
 
-import (
-	"strconv"
-	"strings"
-)
-
 func NumberOfDifferentIntegers(word string) int {
-	uniqueints := make(map[int]bool)
-	var placeholder string
-	for _, k := range strings.Split(word, "") {
-		_, err := strconv.Atoi(k)
-		if err != nil {
-			if placeholder != "" {
-				key, _ := strconv.Atoi(placeholder)
-				uniqueints[key] = true
-				placeholder = ""
+	// map for storing the number
+	m := make(map[string]struct{})
+	// stack for storing the number digit by digit
+	stack := ""
+
+	// iterates the word
+	for i, w := range word {
+		// if digit
+		if isDigit(w) {
+			// if stack is 0, it's mean the number is leading zero
+			// e.g. 001 then remove the 0
+			if stack == "0" {
+				stack = ""
 			}
-			continue
-		} else {
-			placeholder += k
+
+			// push digit on the stack
+			stack += string(w)
+
+			// if current index isn't last index, continue iterates
+			if i != len(word)-1 {
+				continue
+			}
 		}
+
+		// starting this will execute if non digit number found
+		// if stack is empty, then no number need to store
+		if stack == "" {
+			continue
+		}
+
+		// store to the map, reset the stack
+		m[stack] = struct{}{}
+		stack = ""
 	}
-	if placeholder != "" {
-		key, _ := strconv.Atoi(placeholder)
-		uniqueints[key] = true
-		placeholder = ""
-	}
-	return len(uniqueints)
+
+	// count number of digit
+	return len(m)
+}
+
+// isDigit checks the rune is digit or not
+func isDigit(r rune) bool {
+	return '0' <= r && r <= '9'
 }
