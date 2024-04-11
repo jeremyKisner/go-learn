fd9c213f0cb476ee0971b2b6dbaed1284576ce4d
Jeremy Kisner <kisner.jeremy@protonmail.com>
2024-04-10 22:15:00 -0400

    first pass

--- leetcode/easy/numberofints.go
+++ leetcode/easy/numberofints.go
@@ -1,6 +1,27 @@
 package leetcode
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func NumberOfDifferentIntegers(word string) int {
-	var res int
-	return res
+	uniqueints := make(map[string]bool)
+	var placeholder string
+	for i, k := range strings.Split(word, "") {
+		fmt.Println(i, k)
+		parsed, err := strconv.Atoi(k)
+		if err != nil {
+			if placeholder != "" {
+				uniqueints[placeholder] = true
+				placeholder = ""
+			}
+			continue
+		} else {
+			fmt.Println("parsed", parsed)
+			placeholder += k
+		}
+	}
+	return len(uniqueints)
 }
