07fdc7889b175b4c56040d8921e58e4f0d3e241f
Jeremy Kisner <kisner.jeremy@protonmail.com>
2024-07-11 21:47:37 -0400

    refactor function

--- leetcode/easy/twoSum.go
+++ leetcode/easy/twoSum.go
@@ -1,16 +1,21 @@
 package leetcode
 
-// TwoSum will interate an array of ints to figure out what two equal target
+// TwoSum returns the ints that equal the target.
 func TwoSum(nums []int, target int) []int {
 	result := []int{}
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				result = append(result, i, j)
-				break
-			}
-		}
+		result = calculateTarget(i, nums, target, result)
 		if len(result) != 0 && i != 0 {
+			break
+		}
+	}
+	return result
+}
+
+func calculateTarget(i int, nums []int, target int, result []int) []int {
+	for j := i + 1; j < len(nums); j++ {
+		if nums[i]+nums[j] == target {
+			result = append(result, i, j)
 			break
 		}
 	}
